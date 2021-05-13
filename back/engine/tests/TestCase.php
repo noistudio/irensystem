ea29e982f56f6a119a0c797a7c14f2b5e9795358
NoiStudio <info@noibiz.com>
2021-05-13 23:29:32 +0400

    Исправление бага с удалением всех данных из конфигурационных файлов

--- back/engine/tests/TestCase.php
+++ back/engine/tests/TestCase.php
@@ -24,21 +24,7 @@ abstract class TestCase extends BaseTestCase
     protected function callBeforeApplicationDestroyedCallbacks()
     {
 
-        $route = \routes\models\RoutesModel::get("/phpunit");
-        if (isset($route) and is_array($route) and isset($route['id'])) {
-            \db\JsonQuery::delete((int)$route['id'], "id", "routes");
-        }
-
-        if (class_exists("\\mg\\config")) {
-            \mg\core\CollectionModel::delete("phpunit");
-        }
-
-        if (!\core\ManagerConf::isOnlyMongodb()) {
-            \db\SqlQuery::delete("elfinder_files", null);
-            \content\models\TableConfig::delete('phpunit_mysql');
-        }
-
-
+         
         parent::callBeforeApplicationDestroyedCallbacks();
     }
 
